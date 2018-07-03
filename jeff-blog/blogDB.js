46a2b3ae45178aff8a2e1f73818f41ec8a722209
Margaret Tanzosh <margarettanzosh@MargareComputer.home>
2018-07-02 21:56:28 -0400

    finishing touches

---
+++ jeff-blog/blogDB.js
@@ -0,0 +1,87 @@
+// Initialize Firebase
+  const config = {
+    apiKey: "AIzaSyDNAYGyzlvOhWXkzpda_eXjOezBJfqiLco",
+    authDomain: "jwblog-f614f.firebaseapp.com",
+    databaseURL: "https://jwblog-f614f.firebaseio.com",
+    projectId: "jwblog-f614f",
+    storageBucket: "jwblog-f614f.appspot.com",
+    messagingSenderId: "282245716688"
+  };
+  const fb_app = firebase.initializeApp(config);
+  const db = fb_app.database();
+
+  let postRef = db.ref("posts");
+
+  function eventMonth(date) {
+    let month = parseInt(date.substring(5, 7));
+    return month;
+  }
+
+  function eventDay(date) {
+    let day = parseInt(date.substring(8, 10));
+    return day;
+  }
+
+  function eventYear(date) {
+    let year = parseInt(date.substring(0, 4));
+    return year;
+  }
+
+
+  const monthName = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+  function niceDate(date) {
+    let day = eventDay(date);
+    let month = monthName[eventMonth(date) - 1];
+    let year = eventYear(date);
+    return month + ' ' + day + ', ' + year;
+  }
+
+//   function getPosts() {
+//     let posts = [];
+//
+//     postRef.on("value", function(snapshot){
+//       let data = snapshot.val();
+//       console.log(data);
+//
+//       for (let key in data) {
+//         posts.push(data[key])
+//       }
+//
+//       console.log(posts);
+//       return posts;
+//
+//       // create html in #postings
+//       // get most recent post first
+//       // let start = posts.length - 1;
+//       // let html = getHTML(1);
+//       //
+//       // $('#postings').html(html);
+//       // let numPosts = 1;
+//
+//
+//   })
+// }
+
+//   function getHTML(numPosts) {
+//     let html = '';
+//     for (let i = posts.length - 1, end = posts.length - 1 - numPosts; i > end; i--) {
+//       html += '<h4>' + posts[i].headline + '</h4>' +
+//                   '<small>' + posts[i].date + '</small>' +
+//                   '<div class="blog-space">' + posts[i].post + '</div>';
+//     }
+//
+//     if (posts.length > numPosts) {
+//       html += '<div id="more-blog" class="blog-link"><a href="#/">More</a></div>';
+//     }
+//
+//     $('#more-blog').click(function() {
+//         console.log("clicked");
+//         numPosts++;
+//         html = getHTML(numPosts);
+//         $('#postings').html(html);
+//     })
+//
+//     return html;
+//   }
+// }
--- jeff-portfolio/js/blog_db.js
+++ jeff-portfolio/js/blog_db.js
@@ -12,6 +12,30 @@
 
   let postRef = db.ref("posts");
 
+  function eventMonth(date) {
+    let month = parseInt(date.substring(5, 7));
+    return month;
+  }
+
+  function eventDay(date) {
+    let day = parseInt(date.substring(8, 10));
+    return day;
+  }
+
+  function eventYear(date) {
+    let year = parseInt(date.substring(0, 4));
+    return year;
+  }
+
+  const monthName = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+  function niceDate(date) {
+    let day = eventDay(date);
+    let month = monthName[eventMonth(date) - 1];
+    let year = eventYear(date);
+    return month + ' ' + day + ', ' + year;
+  }
+
   getPosts();
 
   function getPosts() {
@@ -36,22 +60,23 @@
       let numPosts = 1;
 
       $('#more-blog').click(function() {
-        console.log("clicked");
-        html = getHTML(numPosts + 1);
-        $('#postings').html(html);
+          console.log("clicked");
+          numPosts++;
+          html = getHTML(numPosts);
+          $('#postings').html(html);
+      })
     })
-  })
 
   function getHTML(numPosts) {
     let html = '';
     for (let i = posts.length - 1, end = posts.length - 1 - numPosts; i > end; i--) {
       html += '<h4>' + posts[i].headline + '</h4>' +
-                  '<small>' + posts[i].date + '</small>' +
+                  '<small>' + niceDate(posts[i].date) + '</small>' +
                   '<div class="blog-space">' + posts[i].post + '</div>';
     }
 
-    if (posts.length > numPosts) {
-      html += '<div id="more-blog" class="blog-link"><a href="#/">More</a></div>';
+    if (posts.length <= numPosts) {
+      $('#more-blog').html('');
     }
 
     return html;
